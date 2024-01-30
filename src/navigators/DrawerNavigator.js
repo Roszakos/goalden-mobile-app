06fc8e43eb4ac94ef0df41cfc4484ffd1b871206
Roszakos <139218017+Roszakos@users.noreply.github.com>
2024-01-30 19:10:37 +0100

    Delete Stack Navigator, rely fully on drawer navigator for better performance

--- src/navigators/DrawerNavigator.js
+++ src/navigators/DrawerNavigator.js
@@ -1,39 +1,57 @@
+import { TouchableWithoutFeedback, Text } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import GoalsStackNavigator from './GoalsStackNavigator';
 import TodayPlanScreen from '../screens/drawer/TodayPlanScreen';
 import GoalsOrderPicker from '../components/GoalsOrderPicker';
 import GoalListGroupContextProvider from '../contexts/GoalListGroupContext';
+import AddNewGoalScreen from '../screens/goals-stack/AddNewGoalScreen';
+import GoalsTabNavigator from './GoalsTabNavigator';
+import GoalListContextProvider from '../contexts/GoalListContext';
+
 
 const Drawer = createDrawerNavigator();
 
 export default function DrawerNavigator() {
   return (
     <GoalListGroupContextProvider>
-    <Drawer.Navigator 
-      initialRouteName="GoalList"
-      screenOptions={{
-        drawerStyle: {
-          backgroundColor: '#d49b39',
-          width: 240,
-        },
-        swipeEdgeWidth: 100
-      }}
-    >
-      <Drawer.Screen 
-        name="GoalStackNavigator" 
-        component={GoalsStackNavigator} 
-        options={{ 
-          title: 'Your Goals', 
-          drawerLabel: 'My Goals',
-          headerRight: () => <GoalsOrderPicker />
-        }}
-      />
-      <Drawer.Screen 
-        name="TodayPlan" 
-        component={TodayPlanScreen} 
-        options={{ title: 'Today\'s plan'}}
-      />
-    </Drawer.Navigator>
+      <GoalListContextProvider>
+        <Drawer.Navigator 
+          initialRouteName="GoalList"
+          screenOptions={{
+            drawerStyle: {
+              backgroundColor: '#d49b39',
+              width: 240,
+            },
+            swipeEdgeWidth: 100
+          }}
+        >
+          <Drawer.Screen 
+            name="GoalList" 
+            component={GoalsTabNavigator} 
+            options={{ 
+              title: 'Your Goals', 
+              drawerLabel: 'My Goals',
+              headerRight: () => <GoalsOrderPicker />
+            }}
+          />
+          <Drawer.Screen 
+            name="AddNewGoal" 
+            component={AddNewGoalScreen} 
+            options={({route}) => ({ 
+              title: route.params ? route.params.headerTitle : 'Set new goal', 
+              drawerItemStyle: {display: 'none'},
+              swipeEnabled: false,
+              // headerRight: () => {
+              //     <Text>X</Text>
+              // }
+            })}
+          />  
+          <Drawer.Screen 
+            name="TodayPlan" 
+            component={TodayPlanScreen} 
+            options={{ title: 'Today\'s plan'}}
+          />
+        </Drawer.Navigator>
+      </GoalListContextProvider>
     </GoalListGroupContextProvider>
   );
 }
--- src/screens/goals-stack/GoalListScreen.js
+++ src/screens/goals-stack/GoalListScreen.js
@@ -75,7 +75,8 @@ export default function GoalListScreen(props) {
                     </View>
                     <TouchableHighlight 
                       onPress={() => {
-                        DeviceEventEmitter.emit("event.changeDrawerNavigator", { shouldBeShown: false, enableSwipe: false });
+                        //DeviceEventEmitter.emit("event.changeDrawerNavigator", { shouldBeShown: false, enableSwipe: false });
+                        //props.navigation.setOptions({title: 'Edit Goal'});
                         props.navigation.navigate("AddNewGoal", {goal: goal, action: 'edit', headerTitle: 'Edit goal'});
                       }}
                       style={styles.listItemTouchable} 
